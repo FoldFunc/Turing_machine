3eea817b030f554c53fd56f55a6248bda96a9c8a
FoldFunc <fold.func@gmail.com>
2025-05-10 09:39:56 +0200

    Now machine takes instructions if it sees zero and if it sees one

--- turing/src/main.rs
+++ turing/src/main.rs
@@ -2,10 +2,21 @@ use std::io;
 fn main() {
     let mut tape = make_tape(200);
     let instr_count: i32 = count_of_instructions();
-    let instr: Vec<String> = get_instructions(instr_count);
-    println!("Instructions: {:?}", instr);
+    let instr: Vec<(String, String)> = get_instructions(instr_count);
+    println!("{:?}", instr);
+    let instr_good: bool = validate_instructions(&instr);
 }
 
+fn validate_instructions(instructions: &Vec<(String, String)>) -> bool {
+    for (ins_0, ins_1) in instructions {
+        println!("Instructions if 0: {}", ins_0);
+        println!("Instructions if 1: {}", ins_1);
+    }
+    return true;
+}
+
+
+// Takes a String and asks user to enter something in command line
 fn input_from_user(input_text: String) -> String{
     let mut input = String::new();
     println!("{}", input_text);
@@ -15,15 +26,19 @@ fn input_from_user(input_text: String) -> String{
         .expect("Failed to read user input!");
     input.trim().to_string()
 }
-fn get_instructions(ici: i32) -> Vec<String> {
-    let mut instructions: Vec<String> = Vec::new();
+// Gets a specific instruction from the user
+fn get_instructions(ici: i32) -> Vec<(String, String)> {
+    let mut instructions = Vec::new();
     for _i in 0..ici{
-        let input = String::from("Give me the instruction for the turing machine: ");
-        let instruction = input_from_user(input);
-        instructions.push(instruction);
+        let input = String::from("Give me the instruction for the turing machine if machine sees 0: ");
+        let instruction_0 = input_from_user(input);
+        let input = String::from("Give me the instruction for the turing machine if machine sees 1: ");
+        let instruction_1 = input_from_user(input);
+        instructions.push((instruction_0, instruction_1))
     }
     instructions
 }
+// Gets amount of instructions that the turing machine will use
 fn count_of_instructions() -> i32 {
     let input = String::from("How many instruction has the turing machine: ");
     let instruction_count = input_from_user(input);
@@ -31,7 +46,7 @@ fn count_of_instructions() -> i32 {
     let instruction_count_int = instruction_count.parse::<i32>().unwrap();
     instruction_count_int
 }
-
+// Makes a tape of zeros to turing machine to edit
 fn make_tape(length: i32) -> Vec<i32> {
     let mut tape = Vec::new();
     for _i in 1..length{
